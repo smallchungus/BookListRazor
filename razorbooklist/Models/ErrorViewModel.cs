71a13e31d7a3ec83cbe3ce0351f81e8173425686
WillMACOS <wchen1396@gmail.com>
2023-05-10 21:38:05 -0400

    initial commit

---
+++ razorbooklist/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace razorbooklist.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
