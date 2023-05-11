dc49fa48a0ad8a8f3d46deca654e54c342780bd1
WillMACOS <wchen1396@gmail.com>
2023-05-11 09:37:12 -0400

    can't get nuget package manager working on mac

---
+++ razorbooklist/Data/ApplicationDBContext.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace razorbooklist.Data
+{
+	public class ApplicationDBContext
+	{
+		public ApplicationDBContext()
+		{
+		}
+	}
+}
+
---
+++ razorbooklist/Models/Category.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace razorbooklist.Models
+{
+	public class Category
+	{
+		[Key]
+		public object MyProperty { get; set; }
+		[Required]
+		public String  Name { get; set; }
+
+		public int DisplayOrder { get; set; }
+
+		public DateTime CreatedDateTime { get; set; } = DateTime.Now;
+
+	}
+}
+
